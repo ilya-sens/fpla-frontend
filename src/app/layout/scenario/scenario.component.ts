c99b727063d35d39f1aab0e7a9a80d1257b5ac05
Ilya Ananyev <ilya.ananyev@chamaeleon.de>
2017-08-20 01:30:38 +0200

    update scenarios and scripts

--- src/app/layout/scenario/scenario.component.ts
+++ src/app/layout/scenario/scenario.component.ts
@@ -4,6 +4,7 @@ import {AuthHttp} from 'angular2-jwt'
 import {Observable} from "rxjs/Rx";
 import {GlobalConfig} from "../../global";
 import {TypeEnum} from "../../shared/modules/editable-element/editable-element.component";
+import {AlertService} from "../../shared/services/alert.service";
 
 @Component({
     selector: 'app-scenario',
@@ -17,7 +18,8 @@ export class ScenarioComponent implements OnInit {
     dateFormat: String = GlobalConfig.DATE_FORMAT;
     typeEnum = TypeEnum;
 
-    constructor(private http: AuthHttp,) {
+    constructor(private http: AuthHttp,
+                private alertService: AlertService) {
         this.loadData();
     }
 
@@ -25,7 +27,7 @@ export class ScenarioComponent implements OnInit {
     }
 
     loadData() {
-        Observable.forkJoin([this.getScenarios(), this.getScripts()]).subscribe(
+        Observable.forkJoin([this.sendGetScenariosRequest(), this.sendGetScriptsRequest()]).subscribe(
             results => {
                 this.scenarioModels = results[0].data;
                 this.scriptModels = results[1].data;
@@ -39,16 +41,40 @@ export class ScenarioComponent implements OnInit {
         });
     }
 
-    getScenarios() {
+    updateScenario(scenario: ScenarioModel) {
+        this.sendUpdateScenarioRequest(scenario).subscribe(result => {
+            this.loadData();
+            this.alertService.success(result.message);
+        });
+    }
+
+    updateScript(script: ScriptModel) {
+        this.sendUpdateScriptRequest(script).subscribe(result => {
+                this.loadData();
+                this.alertService.success(result.message);
+            }
+        )
+    }
+
+    private sendGetScenariosRequest() {
         return this.http.get(GlobalConfig.BASE_API_URL + "/scenarios?populate=scenarioScripts").map((res: Response) => res.json());
     }
 
-    getScripts() {
+    private sendCreateScenarioRequest(scenario) {
+        return this.http.post(GlobalConfig.BASE_API_URL + "/scenarios", scenario).map((res: Response) => res.json());
+    }
+
+    private sendUpdateScenarioRequest(scenario: ScenarioModel) {
+        return this.http.put(GlobalConfig.BASE_API_URL + "/scenarios/" + scenario.id, scenario)
+            .map((res: Response) => res.json());
+    }
+
+    private sendGetScriptsRequest() {
         return this.http.get(GlobalConfig.BASE_API_URL + "/scripts").map((res: Response) => res.json());
     }
 
-    saveData() {
-        this.http.post("http://localhost:8080/api/scenario", {name: "just a script"}).map((res: Response) => res.json());
+    private sendUpdateScriptRequest(script: ScriptModel) {
+        return this.http.put(GlobalConfig.BASE_API_URL + "/scripts/" + script.id, script).map((res: Response) => res.json());
     }
 
     log() {
